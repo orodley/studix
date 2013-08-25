27f3a9c571786f81028f471513575bd7e3c18eb0
Owen Rodley <Strigoides@gmail.com>
2013-08-25 22:11:20 +1200

    Explicitly select location for bochs images Doesn't detect them automatically in the new debug version I compiled

--- make_config_files.sh
+++ make_config_files.sh
@@ -17,6 +17,8 @@ cat > bochsrc <<EOF
 ata0: enabled=1, ioaddr1=0x1f0, ioaddr2=0x3f0, irq=14
 ata0-master: type=cdrom, path="$1.iso", status=inserted
 display_library: sdl
+romimage: file=/usr/share/bochs/BIOS-bochs-latest
+vgaromimage: file=/usr/share/vgabios/vgabios.bin
 boot: cdrom
 magic_break: enabled=1
 EOF
