aec8ab0152f7bf05df8072d83e43fa0a2ff67503
Owen Rodley <Strigoides@gmail.com>
2013-08-25 12:45:06 +1200

    Print interrupt description in ISR handler

--- src/isr.c
+++ src/isr.c
@@ -10,7 +10,30 @@ typedef struct Registers
 		int_no, err, eip, cs, eflags, useresp, ss;
 } Registers;
 
+static const char *interrupt_names[] =
+{
+	"Division by zero",
+	"Debug exception",
+	"Non-maskable interrupt",
+	"Breakpoint exception",
+	"Overflow",
+	"Out of bounds exception",
+	"Invalid opcode",
+	"No coprocessor",
+	"Double fault",
+	"Coprocessor segment overrun",
+	"Bad TSS",
+	"Segment not present",
+	"Stack fault",
+	"General Protection Fault",
+	"Page fault",
+	"Unknown interrupt exception",
+	"Coprocessor fault",
+	"Alignment check exception",
+	"Machine check exception"
+};
+
 void isr_handler(Registers regs)
 {
-	term_printf("Unhandled interrupt: %d", regs.int_no);
+	term_printf("Unhandled interrupt: %s\n", interrupt_names[regs.int_no]);
 }
