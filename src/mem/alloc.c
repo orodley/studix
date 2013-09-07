75ae8cd8fe4de06b43f77ae611b27440182d7b18
Owen Rodley <Strigoides@gmail.com>
2013-09-07 14:06:08 +1200

    Split up alloc() and free() a bit

--- src/mem/alloc.c
+++ src/mem/alloc.c
@@ -9,47 +9,51 @@
 #include "heap.h"
 #include "page.h"
 
+static void make_space(Heap *heap, size_t needed_size)
+{
+	uintptr_t old_end_addr = heap->end_addr;
+	uintptr_t old_length   = old_end_addr - heap->start_addr;
+
+	// Expand the heap!
+	expand(heap, old_length + needed_size);
+	uintptr_t new_length = heap->end_addr - heap->start_addr;
+
+	// Find the last header
+	int32_t   header_index = -1;
+	uintptr_t value        =  0;
+	for (size_t i = 0; i < heap->index.size; i++) {
+		uintptr_t tmp = (uintptr_t)ordered_array_lookup(&heap->index, i);
+
+		if (tmp > value) {
+			value = tmp;
+			header_index++;
+		}
+	}
+
+	if (header_index != -1) {
+		// The last header needs adjusting
+		Header *header = ordered_array_lookup(&heap->index, header_index);
+		header->size  += new_length - old_length;
+
+		make_footer((uintptr_t)header + header->size - sizeof(Footer),
+				header);
+	} else {
+		Header *header =
+			make_header(old_end_addr, new_length - old_length, true);
+
+		make_footer(old_end_addr + header->size - sizeof(Footer), header);
+		ordered_array_insert(&heap->index, (void*)header);
+	}
+}
+
 void *alloc(size_t size, bool align, Heap *heap)
 {
 	// Account for header/footer
 	size_t total_block_size = size + sizeof(Header) + sizeof(Footer);
-
 	int32_t i = find_smallest_hole(total_block_size, align, heap);
 
 	if (i == -1) { // Uh oh, there's no hole big enough!
-		uintptr_t old_end_addr = heap->end_addr;
-		uintptr_t old_length   = old_end_addr - heap->start_addr;
-
-		// Expand the heap!
-		expand(heap, old_length + total_block_size);
-		uintptr_t new_length = heap->end_addr - heap->start_addr;
-
-		// Find the last header
-		int32_t   header_index = -1;
-		uintptr_t value        =  0;
-		for (size_t i = 0; i < heap->index.size; i++) {
-			uintptr_t tmp = (uintptr_t)ordered_array_lookup(&heap->index, i);
-
-			if (tmp > value) {
-				value = tmp;
-				header_index++;
-			}
-		}
-
-		if (header_index != -1) {
-			// The last header needs adjusting
-			Header *header = ordered_array_lookup(&heap->index, header_index);
-			header->size  += new_length - old_length;
-
-			make_footer((uintptr_t)header + header->size - sizeof(Footer),
-				header);
-		} else {
-			Header *header =
-				make_header(old_end_addr, new_length - old_length, true);
-
-			make_footer(old_end_addr + header->size - sizeof(Footer), header);
-			ordered_array_insert(&heap->index, (void*)header);
-		}
+		make_space(heap, total_block_size);
 
 		// Now we have enough space. Try again.
 		return alloc(size, align, heap);
@@ -59,16 +63,13 @@ void *alloc(size_t size, bool align, Heap *heap)
 	uintptr_t orig_hole_pos  = (uintptr_t)orig_hole_header;
 	size_t    orig_hole_size = orig_hole_header->size;
 
-	// Should we split the hole in two?
-	// Is the original hole size - requested size less than the
-	// overhead for a new hole?
+	// Don't split the hole if the other half wouldn't be big enough
 	if (orig_hole_size - total_block_size < sizeof(Header) + sizeof(Footer)) {
 		size += orig_hole_size - total_block_size;
 		total_block_size = orig_hole_size;
 	}
 
-	// If we need to page-align it, then do it, and create a new hole in
-	// the gap
+	// If we need to page-align it, then do, and create a new hole in the gap
 	if (align && ((orig_hole_pos + sizeof(Header)) & 0xFFFFF000) != 0) {
 		uintptr_t new_hole_loc  = orig_hole_pos + PAGE_SIZE -
 			(orig_hole_pos & 0xFFF) - sizeof(Header);
@@ -98,7 +99,7 @@ void *alloc(size_t size, bool align, Heap *heap)
 	// If necessary, split the block in two
 	if (orig_hole_size > total_block_size) {
 		// The new hole sits later in memory than the first half, as we've
-		// potentially need our new block to be page-aligned.
+		// potentially already page-aligned our block
 		// So we now create a new header and footer for this new block
 		Header *new_hole_header = make_header(orig_hole_pos + total_block_size,
 				orig_hole_size - total_block_size, true);
@@ -118,43 +119,24 @@ void *alloc(size_t size, bool align, Heap *heap)
 	return (void*)((uintptr_t)block_header + sizeof(Header));
 }
 
-void free(void *ptr, Heap *heap)
+Header *unify_left(Header *header, Footer *footer)
 {
-	if (ptr == NULL)
-		return;
-
-	// Find the header and footer for the pointer
-	Header *header = (Header*)((uintptr_t)ptr - sizeof(Header));
-	Footer *footer = (Footer*)((uintptr_t)header + header->size - sizeof(Footer));
-
-	// Sanity checks
-	// Check the footer has a correct pointer to the header
-	ASSERT(footer->header == header);
-	// Check the magic numbers are present
-	ASSERT(header->magic == HEAP_MAGIC);
-	ASSERT(footer->magic == HEAP_MAGIC);
-
-	header->is_hole = true;
-
-	// Should we add the header for this pointer to the index?
-	bool add_header = true;
-
-	// Unify left
-	// Check if we're immediately next to another hole on the left
-	Footer *potential_footer = (Footer*)((uintptr_t)header - sizeof(Footer));
+	// Check if we're immediately next to another footer on the right
+	Footer *potential_footer = (Footer*)((uintptr_t)header - sizeof(Header));
 	if (potential_footer->magic == HEAP_MAGIC &&
 			potential_footer->header->is_hole) {
-		// Merge the two holes
 		size_t curr_size = header->size;
 		header           = potential_footer->header;
 		footer->header   = header;
 		header->size    += curr_size;
-
-		add_header = false; // It's already in the index
 	}
 
-	// Unify right
-	// Check if we're immediately next to another hole on the right
+	return header;
+}
+
+Footer *unify_right(Header *header, Footer *footer, Heap *heap)
+{
+	// Check if we're immediately next to another header on the right
 	Header *potential_header = (Header*)((uintptr_t)footer + sizeof(Footer));
 	if (potential_header->magic == HEAP_MAGIC &&
 			potential_header->is_hole) {
@@ -174,6 +156,31 @@ void free(void *ptr, Heap *heap)
 
 		ordered_array_remove(&heap->index, i);
 	}
+
+	return footer;
+}
+
+void free(void *ptr, Heap *heap)
+{
+	if (ptr == NULL)
+		return;
+
+	// Find the header and footer for the pointer
+	Header *orig_header = (Header*)((uintptr_t)ptr - sizeof(Header));
+	Header *header      = orig_header;
+	Footer *footer      = (Footer*)((uintptr_t)header + header->size -
+			sizeof(Footer));
+
+	// Sanity checks
+	// Check the footer has a correct pointer to the header
+	ASSERT(footer->header == header);
+	// Check the magic numbers are present
+	ASSERT(header->magic == HEAP_MAGIC);
+	ASSERT(footer->magic == HEAP_MAGIC);
+
+	header->is_hole = true;
+	header = unify_left( header, footer);
+	footer = unify_right(header, footer, heap);
 
 	// We can contract the heap if this hole is at the end
 	if ((uintptr_t)footer + sizeof(Footer) == heap->end_addr) {
@@ -197,6 +204,8 @@ void free(void *ptr, Heap *heap)
 		}
 	}
 
-	if (add_header)
+	// If our header is still the same as at the start, we need to add it
+	// to the free hole index
+	if (header == orig_header)
 		ordered_array_insert(&heap->index, header);
 }
