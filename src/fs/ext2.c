09787696b2b75eb8cdaaf7c4e3eb717761afebed
Owen Rodley <Strigoides@gmail.com>
2013-10-31 18:33:29 +1300

    inode reading

--- src/fs/ext2.c
+++ src/fs/ext2.c
@@ -15,6 +15,8 @@
 #include "term.h"
 
 static const uint16_t EXT2_SIGNATURE  = 0xEF53;
+static const uint16_t INODE_SIZE      = 128;
+static const uint32_t ROOT_INODE      = 2;
 
 static const size_t SUPERBLOCK_OFFSET = 1024;
 static const size_t SUPERBLOCK_LENGTH = 1024;
@@ -31,7 +33,30 @@ static BGD *bgdt;
 static size_t block_size;
 static size_t num_groups;
 
-void load_superblock()
+static void read_inode(Ext2_inode *inode, uint32_t inode_num)
+{
+	inode_num--; // inode numbers start at 1, change to 0-based index
+	size_t block_group = inode_num / superblock.inodes_per_group;
+
+	// Look up the starting block in the BGDT
+	BGD *bgd = &bgdt[block_group];
+	uint32_t i_table_block = bgd->inode_table_addr;
+
+	// Now figure out how many blocks the inode we want is offset by
+	size_t index           = inode_num % superblock.inodes_per_group;
+	size_t block_offset    = (index * INODE_SIZE) / block_size;
+	size_t offset_in_block = (index * INODE_SIZE) % block_size;
+	size_t block           = i_table_block + block_offset;
+
+	// Read however many sectors we need to, then copy
+	size_t num_sectors = sizeof(Ext2_inode) / SECTOR_SIZE + 1;
+	uint16_t buf[num_sectors * SECTOR_SIZE / 2];
+	read_abs_sectors(block * block_size / SECTOR_SIZE, num_sectors, buf);
+	memcpy(inode, &buf[offset_in_block / 2], sizeof(Ext2_inode));
+}
+
+
+static void load_superblock()
 {
 	uint16_t buf[SUPERBLOCK_LENGTH / 2];
 
@@ -44,13 +69,13 @@ void load_superblock()
 
 	// Print some interesting stuff to check it loaded correctly
 	ASSERT(superblock.signature == EXT2_SIGNATURE);
-	term_printf(" total inodes = 0x%X\n", superblock.total_inodes);
-	term_printf(" total blocks = 0x%X\n", superblock.total_blocks);
-	term_printf(" block size   = %b\n", block_size);
-	term_printf(" num blocks   = %d\n", superblock.total_blocks);
-	term_printf(" blocks/group = %d\n", superblock.blocks_per_group);
-	term_printf(" num groups   = %d\n", num_groups);
-	term_printf(" BGDT size    = %b\n", sizeof(BGD) * num_groups);
+	term_printf(" total inodes    = 0x%X\n", superblock.total_inodes);
+	term_printf(" total blocks    = 0x%X\n", superblock.total_blocks);
+	term_printf(" block size      = %b\n",   block_size);
+	term_printf(" num blocks      = %d\n",   superblock.total_blocks);
+	term_printf(" blocks/group    = %d\n",   superblock.blocks_per_group);
+	term_printf(" inodes/group    = %d\n",   superblock.inodes_per_group);
+	term_printf(" num groups      = %d\n",   num_groups);
 }
 
 static void load_bgdt()
@@ -67,9 +92,15 @@ static void load_bgdt()
 	memcpy(bgdt, buf, bgdt_size);
 }
 
-
 void init_fs()
 {
 	load_superblock();
 	load_bgdt();
+
+	// Read the root inode, just for fun
+	Ext2_inode root_inode;
+	read_inode(&root_inode, ROOT_INODE);
+	term_printf(" / creation time = %d\n",   root_inode.creation_time);
+	term_printf(" / uid           = %d\n",   root_inode.uid);
+	term_printf(" / type & perms  = 0x%X\n", root_inode.type_and_permissions);
 }
--- src/include/ext2.h
+++ src/include/ext2.h
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
 // Structure of an ext2 superblock
-// NOTE: The values in the superblock header are stored little-endian, which
-// happens to work fine for us since we're on x86, but this won't work as is
-// on big-endian architectures!
+// NOTE: The values in the superblock header are stored little-endian and read
+// in from the drive in 16-bit units. This happens to work fine for us since
+// we're on x86, but this won't work as is on big-endian architectures!
 typedef struct Ext2_superblock
 {
 	uint32_t total_inodes;
@@ -33,16 +33,104 @@ typedef struct Ext2_superblock
 	uint16_t res_block_gid;
 } __attribute__ ((packed)) Ext2_superblock;
 
+// Structure of a block group descriptor
 typedef struct BGD
 {
-	uint32_t block_bitmap_id;
-	uint32_t inode_bitmap_id;
-	uint32_t inode_table_id;
+	uint32_t block_bitmap_addr;
+	uint32_t inode_bitmap_addr;
+	uint32_t inode_table_addr;
 	uint16_t free_blocks;
 	uint16_t free_inodes;
 	uint16_t used_dirs;
 	uint16_t pad;
 	uint8_t  bg_reserved[12];
 } __attribute__ ((packed)) BGD;
+
+typedef struct Ext2_inode
+{
+	// Bitwise OR of the two groups of #define's below this struct decl
+	uint16_t type_and_permissions;
+	// User ID of the file's owner
+	uint16_t uid;
+	// Size of the corresponding file in bytes
+	uint32_t size;
+
+	// All the fields below which refer to a time use standard POSIX time
+	uint32_t last_access_time;
+	uint32_t creation_time;
+	uint32_t last_modification_time;
+	uint32_t deletion_time;
+
+	// The group ID of the group that has access to the file
+	uint16_t gid;
+	// How many hard links point to this file. Once it reaches zero, the
+	// blocks occupied by this file are marked as unallocated
+	uint16_t link_count;
+	// How many *sectors* are taken up by this file - not blocks
+	uint32_t sectors_used;
+	// Some bitwise OR combination of the flags listed below
+	uint32_t flags;
+	// Currently unused
+	uint32_t os_specific_val1;
+	// Direct block pointers; pointers to blocks that contain this file's data
+	uint32_t dbp[12];
+	// Indirect block pointer; pointer to a block containing DBPs
+	uint32_t ibp;
+	// Doubly indirect block pointer; pointer to a block containing IBPs
+	uint32_t dibp;
+	// Trebly indirect block pointer; pointer to a block containing DIBPs
+	uint32_t tibp;
+	// File version, used by NFS
+	uint32_t gen_number;
+
+	// As we're only doing version 0 (for now), these two values are reserved
+	uint32_t reserved1;
+	uint32_t reserved2;
+
+	// Location of the file fragment; probably won't be used
+	uint32_t fragment_addr;
+	// Currently unused
+	uint8_t  os_specific_val2[12];
+} __attribute__ ((packed)) Ext2_inode;
+
+// Type of file; occupies the top hex digit of type_and_permissions
+// TODO: Unify this with the vfs stuff in fs.h somehow?
+#define FIFO      0x1000
+#define CHAR_DEV  0x2000
+#define DIRECTORY 0x4000
+#define BLOCK_DEV 0x6000
+#define REG_FILE  0x8000
+#define SYMLINK   0xA000
+#define SOCKET    0xC000
+
+// File permissions: occupies the other 3 hex digits of type_and_permissions
+// This should possibly be moved to <sys/stat.h> once it exists, or perhaps
+// kept separate as it is a FS-dependent thing that happens to be the same in
+// this case, and may be different for some future FS
+#define S_ISUID  04000
+#define S_ISGID  02000
+#define S_ISTICK 01000
+#define S_IRUSR  00400
+#define S_IWUSR  00200
+#define S_IXUSR  00100
+#define S_IRGRP  00040
+#define S_IWGRP  00020
+#define S_IXGRP  00010
+#define S_IROTH  00004
+#define S_IWOTH  00002
+#define S_IXOTH  00001
+
+// Flags; Ext2_inode.flags is a bitwise OR combination of these
+#define SECURE_DELETE    0x00001
+#define UNDELETE         0x00002
+#define COMPRESSED       0x00004
+#define SYNCRONOUS       0x00008
+#define IMMUTABLE        0x00010
+#define APPEND_ONLY      0x00020
+#define DUMP_IGNORE      0x00040
+#define NO_UPDATE_ACCESS 0x00080
+// ...
+// Various other flags are defined that I don't care about for now. I'll bother
+// adding them if I ever support them
 
 void init_fs();
